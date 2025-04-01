30e2184b568242361594d63d42f9c78aec0bb706
Ivan-Pudev <ipudev@abv.bg>
2025-04-01 11:20:15 +0300

    did more work

--- ProjectBuildYourOwnAIChatbotWithGroqAPIC#!/ChatBot/obj/Debug/net9.0/ChatBot.AssemblyInfo.cs
+++ ProjectBuildYourOwnAIChatbotWithGroqAPIC#!/ChatBot/obj/Debug/net9.0/ChatBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d904990aab8bb1dc1ad5fa04b0004dc1973c40a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de31c01066cf8d8889652a5f1a7ab13fbfe79e32")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
